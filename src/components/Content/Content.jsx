44d560caeb797f374fcd73864d4f4cd3ee72f3a1
Salomão Silva <salomaosilval@hotmail.com>
2023-03-07 23:01:04 -0400

    :sparkles: feat: add Cart Route  on Content

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,11 +1,13 @@
 import { Switch, Route } from "react-router-dom";
 
 import { Store } from "../../Pages/Store/Store";
+import { Cart } from "../../Pages/Cart/Cart";
 
 export const Content = () => {
   return (
     <Switch>
       <Route exact path="/" component={Store} />
+      <Route exact path="/cart" component={Cart} />
     </Switch>
   );
 };
